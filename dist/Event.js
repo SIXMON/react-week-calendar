b985132fac33fd6e1b6519d825235e788b119e47
Paul McFreely <paulmcfreely@gmail.com>
2018-04-05 11:52:54 +0200

    Custom components can be passed as objects, and thus have props

--- dist/Event.js
+++ dist/Event.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _react = require('react');
+var _react = require("react");
 
 var _react2 = _interopRequireDefault(_react);
 
-var _propTypes = require('prop-types');
+var _propTypes = require("prop-types");
 
 var _propTypes2 = _interopRequireDefault(_propTypes);
 
@@ -23,44 +23,49 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var propTypes = {
-  start: _propTypes2.default.object.isRequired,
-  end: _propTypes2.default.object.isRequired,
-  value: _propTypes2.default.string.isRequired
+    start: _propTypes2.default.object.isRequired,
+    end: _propTypes2.default.object.isRequired,
+    value: _propTypes2.default.string.isRequired
 };
 
 var Event = function (_React$PureComponent) {
-  _inherits(Event, _React$PureComponent);
+    _inherits(Event, _React$PureComponent);
 
-  function Event() {
-    _classCallCheck(this, Event);
+    function Event() {
+        _classCallCheck(this, Event);
 
-    return _possibleConstructorReturn(this, (Event.__proto__ || Object.getPrototypeOf(Event)).apply(this, arguments));
-  }
-
-  _createClass(Event, [{
-    key: 'render',
-    value: function render() {
-      var _props = this.props,
-          start = _props.start,
-          end = _props.end,
-          value = _props.value;
-
-      return _react2.default.createElement(
-        'div',
-        { className: 'event' },
-        _react2.default.createElement(
-          'span',
-          null,
-          start.format('HH:mm') + ' - ' + end.format('HH:mm')
-        ),
-        _react2.default.createElement('br', null),
-        _react2.default.createElement('br', null),
-        value
-      );
+        return _possibleConstructorReturn(this, (Event.__proto__ || Object.getPrototypeOf(Event)).apply(this, arguments));
     }
-  }]);
 
-  return Event;
+    _createClass(Event, [{
+        key: "render",
+        value: function render() {
+            var _props = this.props,
+                start = _props.start,
+                end = _props.end,
+                value = _props.value;
+
+            return _react2.default.createElement(
+                "div",
+                { className: "event" },
+                _react2.default.createElement(
+                    "span",
+                    null,
+                    "Hello"
+                ),
+                _react2.default.createElement(
+                    "span",
+                    null,
+                    start.format("HH:mm") + " - " + end.format("HH:mm")
+                ),
+                _react2.default.createElement("br", null),
+                _react2.default.createElement("br", null),
+                value
+            );
+        }
+    }]);
+
+    return Event;
 }(_react2.default.PureComponent);
 
 Event.propTypes = propTypes;
